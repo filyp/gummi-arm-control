fix rotation and brightss problems

look.py
@@ -6,13 +6,14 @@ rdmilligan.wordpress.com/2015/07/19/glyph-recognition-using-opencv-and-python/
 """
 
 import cv2
+import os
 # print(cv2.getBuildInformation())
 
 from look_helpers import *
 
 # tweak these
-EDGE_LOWER_THRESHOLD = 50
-EDGE_UPPER_THRESHOLD = 100
+EDGE_LOWER_THRESHOLD = 30
+EDGE_UPPER_THRESHOLD = 90
 substitute_image = cv2.imread('substitute.jpg')
 
 # constants
@@ -23,8 +24,13 @@ BUILTIN_CAMERA = 0
 LOOPBACK_CAMERA = 1
 
 
+# # bind camera to /dev/video1
+# os.spawnl(os.P_NOWAIT,
+#           'gphoto2 --stdout --capture-movie | \
+#                 gst-launch-1.0 fdsrc fd=0 ! decodebin name=dec ! queue ! \
+#                 videoconvert ! tee ! v4l2sink device=/dev/video1')
 cv2.namedWindow("camera")
-vc = cv2.VideoCapture(BUILTIN_CAMERA)        # set to 0 for built in camera
+vc = cv2.VideoCapture(LOOPBACK_CAMERA)
 
 
 is_open = vc.isOpened()
@@ -36,16 +42,15 @@ while is_open:
 
     edges = cv2.Canny(imgray, EDGE_LOWER_THRESHOLD, EDGE_UPPER_THRESHOLD)
     im2, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:100]
 
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:30]
     number_of_detected = 0
     for contour in contours:
         # approximate the contour
         peri = cv2.arcLength(contour, True)
-        approx = cv2.approxPolyDP(contour, 0.03 * peri, True)
+        approx = cv2.approxPolyDP(contour, 0.01 * peri, True)
         if len(approx) != 4:
             continue
-
         topdown_quad = get_topdown_quad(imgray, approx.reshape(4, 2))
         bitmap = cv2.resize(topdown_quad, (5, 5))
 
@@ -56,20 +61,17 @@ while is_open:
                 break
             bitmap = rotate_image(bitmap, 90)
 
-        # cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
         if glyph_found:
             number_of_detected += 1
             cv2.imshow("im", bitmap)
-            # approx = np.roll(approx, rotation_num)
-            frame = add_substitute_quad(frame, substitute_image, approx.reshape(4, 2))
+            frame = overlay(frame, substitute_image, approx, rotation_num)
             last_approx = approx
-            # cv2.drawContours(frame, [approx], -1, (0, 255, 0), 3)
 
     # temporary solution for annoying flickering
     # TODO fix image lingering too long
     if number_of_detected == 0:
         try:
-            frame = add_substitute_quad(frame, substitute_image, last_approx.reshape(4, 2))
+            frame = overlay(frame, substitute_image, last_approx, 0)
         except:
             pass
 

look_helpers.py
@@ -55,34 +55,54 @@ def get_topdown_quad(image, src):
     return warped
 
 
-def add_substitute_quad(image, substitute_quad, dst):
-    # dst (zero-set) and src points
-    dst = order_points(dst)
+def overlay(background, overlay_image, approx, rotation_num):
+    overlay_image = rotate_image(overlay_image, -rotation_num * 90)
 
-    (tl, tr, br, bl) = dst
-    min_x = min(int(tl[0]), int(bl[0]))
-    min_y = min(int(tl[1]), int(tr[1]))
+    overlay_height, overlay_width = overlay_image.shape[:2]
+    background_height, background_width = background.shape[:2]
 
-    for point in dst:
-        point[0] = point[0] - min_x
-        point[1] = point[1] - min_y
+    points = np.asarray(
+        [np.asarray(x[0], dtype=np.float32) for x in approx],
+        dtype=np.float32
+    )
+    points = order_points(points)
 
-    (max_width, max_height) = max_width_height(dst)
-    src = topdown_points(max_width, max_height)
+    input_coordinates = np.asarray(
+        [
+            np.asarray([0, 0], dtype=np.float32),
+            np.asarray([overlay_width, 0], dtype=np.float32),
+            np.asarray([overlay_width, overlay_height], dtype=np.float32),
+            np.asarray([0, overlay_height], dtype=np.float32)
+        ],
+        dtype=np.float32,
+    )
 
-    # warp perspective (with white border)
-    substitute_quad = cv2.resize(substitute_quad, (max_width, max_height))
+    transformation_matrix = cv2.getPerspectiveTransform(
+        np.asarray(input_coordinates),
+        np.asarray(points),
+    )
 
-    warped = np.zeros((max_height, max_width, 3), np.uint8)
-    warped[:, :, :] = 255
+    warped_image = cv2.warpPerspective(
+        overlay_image,
+        transformation_matrix,
+        (background_width, background_height),
+    )
 
-    matrix = cv2.getPerspectiveTransform(src, dst)
-    cv2.warpPerspective(substitute_quad, matrix, (max_width, max_height), warped, borderMode=cv2.BORDER_TRANSPARENT)
+    alpha_channel = np.ones(overlay_image.shape, dtype=np.float)
+    alpha_channel = cv2.warpPerspective(
+        alpha_channel,
+        transformation_matrix,
+        (background_width, background_height),
+    )
 
-    # add substitute quad
-    image[min_y:min_y + max_height, min_x:min_x + max_width] = warped
-
-    return image
+    def normalize(im):
+        min_val = np.min(im.ravel())
+        max_val = np.max(im.ravel())
+        out = (im.astype('float') - min_val) / (max_val - min_val)
+        return out
+    background = normalize(background)
+    warped_image = normalize(warped_image)
+    return (warped_image * alpha_channel) + (background * (1 - alpha_channel))
 
 
 def check_for_pattern(bitmap, glyph_pattern_centre):