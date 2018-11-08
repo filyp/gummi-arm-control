ce07b019b2cb9b4abbe69a0050339511491eec3c
Anna <anna.belowska2@gmail.com>
2018-11-08 14:02:56 +0100

    Fit servos controlling to directions of tendons

--- experiments/test.py
+++ experiments/test.py
@@ -1,25 +1,32 @@
-import csv
 import time
+from src import maestro
+from config.constants import PORT, BAUDRATE
 
-import numpy as np
 
-import look
-import talk
-
-import numpy as np
-import serial
-
-from config.constants import PORT, BAUDRATE, MAX_ANGLE
+def degrees_to_quarter_millis(angle_in_degrees):
+    MAX_ANGLE = 180
+    MIN_MAESTRO = 4000
+    MAX_MAESTRO = 8000
+    ratio = angle_in_degrees / MAX_ANGLE
+    quarter_millis = MIN_MAESTRO + (MAX_MAESTRO - MIN_MAESTRO) * ratio
+    return int(quarter_millis)
 
 
 def main():
-    ser = serial.Serial(PORT, BAUDRATE)
-    servo1 = 180
-    servo2 = 0
+    SERVO1 = 0
+    SERVO2 = 1
+
+    m = maestro.Controller()
+
+    angle1 = 180
+    angle2 = 0
 
     while True:
-        ser.write(b'A' + servo1.to_bytes(1, 'little'))
-        ser.write(b'B' + servo2.to_bytes(1, 'little'))
+        target1 = degrees_to_quarter_millis(angle1)
+        target2 = degrees_to_quarter_millis(angle2)
+
+        m.setTarget(SERVO1, target1)
+        m.setTarget(SERVO2, target2)
         time.sleep(2)
 
 
