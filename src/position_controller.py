d2fc4fcf63f8cb99c2cf33807ca469bdbd9d9ef3
Anna <anna.belowska2@gmail.com>
2018-11-13 19:25:45 +0100

    Fit servos position controller for tendons

--- src/position_controller.py
+++ src/position_controller.py
@@ -1,4 +1,4 @@
-import maestro
+from src import maestro
 
 
 class PositionController:
@@ -10,17 +10,18 @@ class PositionController:
     MAX_ANGLE = 180
     MIN_MAESTRO = 4000
     MAX_MAESTRO = 8000
-    SERVO1 = 0
-    SERVO2 = 1
+    BACK_SERVO = 0
+    FRONT_SERVO = 1
 
     def __init__(self):
         self.maestro = maestro.Controller()
 
-    def get_angle1(self, angle, stiffness):
-        return angle - stiffness
+    def get_back_angle(self, angle, stiffness):
+        return (180 - angle) + stiffness
 
-    def get_angle2(self, angle, stiffness):
-        return angle + stiffness
+    def get_front_angle(self, angle, stiffness):
+        proportional_stiffness = stiffness - 1 if (stiffness != 0) else stiffness
+        return angle - proportional_stiffness
 
     def _angle_valid(self, angle):
         """return if given angle is inside the servos range"""
@@ -33,12 +34,12 @@ class PositionController:
 
     def send(self, arm_angle, stiffness):
         """send given position to the arm"""
-        angle1 = self.get_angle1(arm_angle, stiffness)
-        angle2 = self.get_angle2(arm_angle, stiffness)
+        angle1 = self.get_back_angle(arm_angle, stiffness)
+        angle2 = self.get_front_angle(arm_angle, stiffness)
         if self._angle_valid(angle1) and self._angle_valid(angle2):
             target1 = self.degrees_to_quarter_millis(angle1)
             target2 = self.degrees_to_quarter_millis(angle2)
-            self.maestro.setTarget(self.SERVO1, target1)
-            self.maestro.setTarget(self.SERVO2, target2)
+            self.maestro.setTarget(self.BACK_SERVO, target1)
+            self.maestro.setTarget(self.FRONT_SERVO, target2)
         else:
             raise ValueError
