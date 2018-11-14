34a60e2353f7bab70b2c646119d2f86b8665ec41
Anna <anna.belowska2@gmail.com>
2018-11-14 16:42:57 +0100

    Moved controller initialisation to constructor

--- inter/interpolation_position_controller.py
+++ inter/interpolation_position_controller.py
@@ -12,19 +12,16 @@ from inter.interpolation import *
 class InterpolationPositionController:
     def __init__(self):
         self.interpolator = ServoAngleInterpolator()
+        self.controller = position_controller.PositionController()
 
     def send(self, angle, stiffness):
         servo_angle = self.interpolator.get_servo_angle(angle, stiffness)
-        controller = position_controller.PositionController()
 
         while True:
             try:
-                controller.send(servo_angle, stiffness)
-                # print(servo_angle)
-                # print('sent')
+                self.controller.send(servo_angle, stiffness)
                 break
             except ValueError:
-                print('dupa')
                 # chosen values were out of servos' range, so choose once again
                 pass
 
