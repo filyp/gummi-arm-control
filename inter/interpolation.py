2d9072210e16a41ca74134e50ed3e4c3f0e5dbbb
Anna <anna.belowska2@gmail.com>
2018-11-19 17:33:17 +0100

    Removed evaluation data on grid in interpolation method. Evaluation moved to plot method. Added coefficient to constructor

--- inter/interpolation.py
+++ inter/interpolation.py
@@ -6,10 +6,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.linalg
 
+location = '../data/interpolation'
+
 
 def get_default_file():
-    location = '../data/interpolation'
-    least_timestamp = None
+    latest_timestamp = None
     file_name = None
 
     for file in os.listdir(location):
@@ -18,27 +19,28 @@ def get_default_file():
                 file_name, timestamp_string = file.split('_')
                 t = datetime.datetime.strptime(timestamp_string, "%Y-%m-%d %H:%M:%S")
 
-                if least_timestamp is None:
-                    least_timestamp = t
+                if latest_timestamp is None:
+                    latest_timestamp = t
                 else:
-                    least_timestamp = max((least_timestamp, t))
+                    latest_timestamp = max((latest_timestamp, t))
 
         except Exception as e:
-            print("No files found here!")
+            print("No files found here")
             raise e
-    return file_name + least_timestamp
+    return file_name + latest_timestamp
 
 
 class InterpolationExecutor:
-    def __init__(self, file_path=get_default_file()):
+    def __init__(self, file_name=get_default_file()):
         self.angle = []
         self.stiffness = []
         self.camera = []
-        self.function = None
+        self.coeffs = None
 
-        self.import_from_csv(file_path)
+        self.import_from_csv(file_name)
 
-    def import_from_csv(self, file_path):
+    def import_from_csv(self, file_name):
+        file_path = location + '/' + file_name
         input_file = csv.DictReader(open(file_path))
 
         for row in input_file:
@@ -46,53 +48,52 @@ class InterpolationExecutor:
             self.stiffness.append(int(row['stiffness']))
             self.camera.append(float(row['camera']))
 
-    def interpolate(self):
-        degree = 3
-
-        # np.vander(input array, number of columns in the output)
-        A_angle = np.vander(self.angle, degree)
-        A_stiffness = np.vander(self.stiffness, degree)
-        A = np.hstack((A_angle, A_stiffness))
-
-        # Solve for a least squares estimate
-        (coeffs, residuals, rank, sing_vals) = np.linalg.lstsq(A, self.camera)
-
-        # Extract coefficients and create polynomials in x and y
-        xcoeffs = coeffs[0:degree]
-        ycoeffs = coeffs[degree:2 * degree]
-        print(ycoeffs)
-
-        fx = np.poly1d(xcoeffs)
-        fy = np.poly1d(ycoeffs)
-
-        self.function = fx
-        Z = np.array([fx(a) + fy(s) for (a, s) in zip(self.angle, self.stiffness)])
-
-        new_fx = lambda x: fx(x) + 0.9335
-        return new_fx, Z
+    # def interpolate(self):
+    #     degree = 3
+    #
+    #     # np.vander(input array, number of columns in the output)
+    #     A_angle = np.vander(self.angle, degree)
+    #     A_stiffness = np.vander(self.stiffness, degree)
+    #     A = np.hstack((A_angle, A_stiffness))
+    #
+    #     # Solve for a least squares estimate
+    #     (coeffs, residuals, rank, sing_vals) = np.linalg.lstsq(A, self.camera)
+    #
+    #     # Extract coefficients and create polynomials in x and y
+    #     xcoeffs = coeffs[0:degree]
+    #     ycoeffs = coeffs[degree:2 * degree]
+    #     print(ycoeffs)
+    #
+    #     fx = np.poly1d(xcoeffs)
+    #     fy = np.poly1d(ycoeffs)
+    #
+    #     self.function = fx
+    #     Z = np.array([fx(a) + fy(s) for (a, s) in zip(self.angle, self.stiffness)])
+    #
+    #     new_fx = lambda x: fx(x) + 0.9335
+    #     return new_fx, Z
 
     def interpolate_2(self):
         # c_ method added items along second axis
         data = np.c_[self.angle, self.stiffness, self.camera]
 
-        x_range = np.arange(0, 200, 20)
-        y_range = np.arange(0, 6, 1)
-        X, Y = np.meshgrid(x_range, y_range)
-        #
         # best-fit quadratic curve
         A = np.c_[np.ones(data.shape[0]), data[:, :2], np.prod(data[:, :2], axis=1), data[:, :2] ** 2]
-        C, _, _, _ = scipy.linalg.lstsq(A, data[:, 2])
+        self.coeffs, _, _, _ = scipy.linalg.lstsq(A, data[:, 2])
 
-        # evaluate it on a grid
-        Z = C[4] * X ** 2. + C[5] * Y ** 2. + C[3] * X * Y + C[1] * X + C[2] * Y + C[0]
-        result_fxy = lambda x, y: C[4] * x ** 2. + C[5] * y ** 2. + C[3] * x * y + C[1] * x + C[2] * y + C[0]
+        result_fxy = lambda x, y: self.coeffs[4] * x ** 2. + self.coeffs[5] * y ** 2. + self.coeffs[3] * x * y + self.coeffs[1] * x + self.coeffs[2] * y + self.coeffs[0]
 
-        return result_fxy, Z
+        return result_fxy
 
-    def plot(self, Z):
+    def plot(self):
+        _ = self.interpolate_2()
+
         x_range = np.arange(0, 200, 20)
         y_range = np.arange(0, 6, 1)
         X, Y = np.meshgrid(x_range, y_range)
+
+        # evaluate it on a grid
+        Z = self.coeffs[4] * X ** 2. + self.coeffs[5] * Y ** 2. + self.coeffs[3] * X * Y + self.coeffs[1] * X + self.coeffs[2] * Y + self.coeffs[0]
 
         # plots 3D chart
         fig = plt.figure()
--- inter/servo_angle_interpolator.py
+++ inter/servo_angle_interpolator.py
@@ -9,7 +9,7 @@ class ServoAngleInterpolator:
     def __init__(self):
         executor = InterpolationExecutor()
         # executor.import_from_csv()
-        self.f, _ = executor.interpolate_2()
+        self.f = executor.interpolate_2()
 
     def get_servo_angle(self, result_angle, stiffness):
         x_a = scipy.optimize.fsolve(lambda x: self.f(x, stiffness) - int(result_angle), np.array([0]))
