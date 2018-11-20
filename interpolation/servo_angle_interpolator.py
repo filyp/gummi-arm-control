f56d769f20d2240480660f1ded5b6e2e7c5cb69f
Anna <anna.belowska2@gmail.com>
2018-11-20 23:57:38 +0100

    Refactored

--- interpolation/servo_angle_interpolator.py
+++ interpolation/servo_angle_interpolator.py
@@ -8,8 +8,8 @@ from interpolation.interpolation import InterpolationExecutor
 class ServoAngleInterpolator:
     def __init__(self):
         executor = InterpolationExecutor()
-        executor.import_from_csv()
-        self.f, _ = executor.interpolate_2()
+        # executor.import_from_csv()
+        self.f = executor.interpolate_2()
 
     def get_servo_angle(self, result_angle, stiffness):
         x_a = scipy.optimize.fsolve(lambda x: self.f(x, stiffness) - int(result_angle), np.array([0]))
