deeba19b57ce55f693c8c54409f854299fcc01f3
Anna <anna.belowska2@gmail.com>
2018-12-08 21:46:10 +0100

    PID controller can now use non trivial stiffness

--- PID/pid_script.py
+++ PID/pid_script.py
@@ -1,15 +1,29 @@
 from PID.pid_controller import PIDController
 from src.look import PositionDetector
 from src.position_controller import PositionController
+from src.control import maestro
+import numpy as np
+
 
 STIFFNESS = 10
 TARGET_ANGLE = 90
-STARTING_POSITION = 0
+STIFFNESS_FUNCTION = lambda x: x**2 # stub function of stiffness course
 
-raw_controller = PositionController()
+maestro = maestro.Controller()
 position_detector = PositionDetector(1.0)
+raw_controller = PositionController()
+
+starting_servo_position = raw_controller.get_servo_position()
+starting_position = int(position_detector.get_angle())
+
+direction = np.sign(TARGET_ANGLE - starting_position)
+tick = 2
+
+angle_grid = range(starting_position, TARGET_ANGLE + direction, tick * direction)
+stiffness_grid = [STIFFNESS_FUNCTION(a) for a in angle_grid]
+
+if direction == -1:
+    stiffness_grid = reversed(stiffness_grid)
 
 pid_controller = PIDController(position_detector, raw_controller)
-raw_controller.send(STARTING_POSITION, STIFFNESS)
-
-pid_controller.control(TARGET_ANGLE, STARTING_POSITION, STIFFNESS)
+pid_controller.control(TARGET_ANGLE, starting_servo_position, stiffness_grid, tick)
