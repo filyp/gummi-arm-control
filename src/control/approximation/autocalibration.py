dffa17df76716fbc44bc9a0d3ef05ef12049cc75
Anna <anna.belowska2@gmail.com>
2018-12-10 17:17:58 +0100

    Removed creating raw controller and position detector in autocalibration. Added class for autocalibration

--- src/control/approximation/autocalibration.py
+++ src/control/approximation/autocalibration.py
@@ -1,55 +1,25 @@
-import os
-
-from src.control.approximation.approximating_function_finder import ApproximatingFunctionFinder
-from src.position_detection.position_detector import PositionDetector
-import src.benchmark.collect_data_for_approximation as interpolation_experiment
+import src.benchmark.collect_data_for_approximation as collect_data_for_approximation
 
 
-print("welcome in  auto-calibration mode. Please, enjoy your arm!")
-print("""Put camera in front of arm's glyphs and make sure that all four glyphs in max/min arm position are visible""")
+class Autocalibration:
+    def __init__(self, raw_controller, position_detector):
+        self.raw_controller = raw_controller
+        self.position_detector = position_detector
 
-position_detector = PositionDetector(1)
-try:
-    position_detector.connect_camera()
-    print('Camera connected')
-    position_detector.kill()
-    position_detector.join()
+    def run(self):
+        print("welcome in  auto-calibration mode. Please, enjoy your arm!")
+        print("""Put camera in front of arm's glyphs and make sure that all four glyphs in max/
+        min arm position are visible""")
 
-    # data_for_approximation experiment
-    # ask about custom duration of experiment
-    try:
-        duration = int(input("""Put experiment duration in seconds. Default is 2"""))
-        print('Started data_for_approximation experiment with duration: %'.format(duration))
-        approximation_experiment.start()
-    except ValueError:
-        print('Started data_for_approximation experiment with default iteration number')
-        approximation_experiment.start()
-        pass
+        # ask about custom duration of experiment
+        try:
+            duration = int(input("""Put experiment duration in seconds. Default is 2"""))
+            print('Started data_for_approximation experiment with duration: %'.format(duration))
+            collect_data_for_approximation.start(self.raw_controller, self.position_detector, running_time=duration)
+        except ValueError:
+            print('Started data_for_approximation experiment with default duration')
+            collect_data_for_approximation.start(self.raw_controller, self.position_detector)
+            pass
 
-    print("Experiment finished. Results in data dir")
+        print("Experiment finished. Results in data dir")
 
-    # drawing data_for_approximation data
-    # ask about source data_for_approximation data file
-    executor = None
-    try:
-        file_name = input("""For drawing put data .csv filename from data/approximation/data_for_approximation dir. 
-        Default is the latest one""")
-
-        # TODO change path
-        if os.path.isfile('../data/data_for_approximation/' + file_name):
-            executor = ApproximatingFunctionFinder(file_name)
-        else:
-            print('Used default data_for_approximation data .csv file')
-            executor = ApproximatingFunctionFinder()
-    except IOError:
-        print('Cannot open file')
-        pass
-    # plotting and saving chart
-    # TODO change to saving 3D chart automatically
-    executor.plot_approximating_function()
-
-except IOError:
-    print('No camera found')
-finally:
-    position_detector.kill()
-    position_detector.join()
