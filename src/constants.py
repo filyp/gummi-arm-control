9186a09e34711c418b89b75e2ed6330f2cb22ff1
Anna <anna.belowska2@gmail.com>
2018-12-10 22:21:49 +0100

    Added path to approximation stats result dir to constants

--- src/constants.py
+++ src/constants.py
@@ -6,6 +6,7 @@ project_path = os.path.join(os.path.dirname(__file__), '../')
 data_for_approximation = 'data/data_for_approximation/*'
 approximating_functions = 'data/functions/'
 approximation_data = 'data/data_for_approximation/'
+approximation_results = 'data/experiments_results/approximation_experiment/results/'
 config_files = 'config/'
 
 
@@ -16,6 +17,7 @@ def get_absolute_path(path):
 DATA_FOR_APPROXIMATION = get_absolute_path(data_for_approximation)
 APPROXIMATING_FUNCTIONS_PATH = get_absolute_path(approximating_functions)
 APPROXIMATION_DATA_PATH = get_absolute_path(approximation_data)
+APPROXIMATION_RESULTS_PATH = get_absolute_path(approximation_results)
 DEFAULT_FUNCTION = 'default.pickle'
 CONFIG_FILES = get_absolute_path(config_files)
 DEFAULT_CONFIG = 'default.json'
