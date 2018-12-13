af6463987cd10a8909e190e6b19fc3dbef301c6e
Anna <anna.belowska2@gmail.com>
2018-12-13 13:13:32 +0100

    Separated loading and executing tests

--- src/benchmark/test_utils/test_config_loader.py
+++ src/benchmark/test_utils/test_config_loader.py
@@ -3,21 +3,34 @@ from ruamel.yaml import YAML
 from src.benchmark import accuracy_experiment
 from src.benchmark.test_utils.accuracy_stats import AccuracyStats
 
+# todo add to constants
 FILE_LOCATION = "../../../config/tests_config"
 
-yaml = YAML(typ='safe')
-my_dict = yaml.load(open(FILE_LOCATION))
 
-if my_dict['accuracy_experiment'] is not None:
-    print('Started examining data_for_approximation accuracy_experiment')
-    accuracy_experiments = []
-    current_experiment = my_dict['accuracy_experiment']
-    for key in current_experiment:
-        accuracy_experiment.start(current_experiment[key]['angle'], current_experiment[key]['stiffness'])
-    accuracy_stats = AccuracyStats()
-    accuracy_stats.generate_statistics()
+class TestConfigurationLoader:
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def load_test_configuration(self):
+        yaml = YAML(typ='safe')
+        config_dict = yaml.load(open(self.file_name))
+
+        return config_dict
 
 
-if my_dict['dupa_experiment'] is not None:
-    print('dupa')
+class TestExecutor:
+    def __init__(self, file_name=FILE_LOCATION):
+        self.config = TestConfigurationLoader(file_name).load_test_configuration()
 
+    def start(self):
+
+        if self.config['accuracy_experiment'] is not None:
+            print('Started examining data_for_approximation accuracy_experiment')
+            current_experiment = self.config['accuracy_experiment']
+            for _, experiment in current_experiment.items():
+                accuracy_experiment.start(experiment['angle'], experiment['stiffness'])
+            accuracy_stats = AccuracyStats()
+            accuracy_stats.generate_statistics()
+
+        # you can add another test strategies here
+        # if ... is not None:
