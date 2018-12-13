d6653ef77766feca0719a0565408370829ba60d8
Filip <filipsondej@protonmail.com>
2018-12-13 16:35:14 +0100

    implement PID + approximation control

--- src/control/PID_regulator/pid_controller.py
+++ src/control/PID_regulator/pid_controller.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from src.control.PID_regulator.pid import PID
 
 # todo change to 1 - 0.5 % of first error rate
@@ -7,16 +9,39 @@ THRESHOLD = 0.2
 
 
 class PIDController:
-    def __init__(self, position_detector, raw_controller, stiffness_function, P=0.7, I=0.2, D=0.2):
+    def __init__(self, position_detector, raw_controller, stiffness_function,
+                 P=0.7, I=0.2, D=0.2, interception_moment=None):
         self.pid = PID(P=P, I=I, D=D)
         self.position_detector = position_detector
         self.raw_controller = raw_controller
         # todo evaluate function from string
         self.stiffness_function = stiffness_function
+        self.interception_moment = interception_moment
 
     # def get_current_stiffness_index(self, current_angle, tick):
     #     index = int(current_angle / tick)
     #     return index
+
+    def wait_for_interception(self, starting_angle, target_angle):
+        """Wait until arm is close enough to target angle.
+
+        Reads current arm angle from PositionDetector.
+        If the arm travelled more than some ratio of the distance
+        between starting_angle and target_angle, method returns.
+        That ratio must be specified in self.interception_moment.
+
+        Args:
+            starting_angle:   angle in which arm started
+            target_angle:     angle that we want to move the arm to
+
+        """
+        distance = abs(target_angle - starting_angle)
+        while True:
+            current_angle = self.position_detector.get_angle()
+            movement_completion = 1 - abs(current_angle - target_angle) / distance
+            if movement_completion > self.interception_moment:
+                return
+            sleep(0.001)
 
     def control(self, target_angle, starting_servo_angle, target_stiffness, starting_position):
         self.pid.set_point(target_angle)
--- src/control/position_controller.py
+++ src/control/position_controller.py
@@ -94,8 +94,11 @@ class PositionController:
     def _load_pid_module(self):
         # self.connect_camera()
         pid_params = self.config['PID']
+        def stiffness_function(x):
+            return x**2
         self.pid = PIDController(self.position_detector,
                                  self.raw_controller,
+                                 stiffness_function,
                                  **pid_params)
 
     def connect_camera(self, reconnect_if_exists=False):
@@ -140,11 +143,19 @@ class PositionController:
             servo_angle = self.approximator.get_servo_angle(angle, stiffness)
             self.raw_controller.send(servo_angle, stiffness)
         elif self.modules == {'approximation', 'PID'}:
-            # todo implement
-            raise NotImplementedError
+            starting_servo_position = self.raw_controller.get_servo_position()
+            starting_arm_position = int(self.position_detector.get_angle())
+
+            servo_angle = self.approximator.get_servo_angle(angle, stiffness)
+            self.raw_controller.send(servo_angle, stiffness)
+            self.pid.wait_for_interception(starting_arm_position, angle)
+            self.pid.control(angle, starting_servo_position,
+                             stiffness, starting_arm_position)
+
         elif self.modules == {'PID'}:
             starting_servo_position = self.raw_controller.get_servo_position()
-            starting_position = int(self.position_detector.get_angle())
-            self.pid.control(angle, starting_servo_position, stiffness, starting_position)
+            starting_arm_position = int(self.position_detector.get_angle())
+            self.pid.control(angle, starting_servo_position,
+                             stiffness, starting_arm_position)
         elif not self.modules:
-            logging.error('to send, you must first load some configuration')
+            logging.error('To send, you must first load some configuration.')
